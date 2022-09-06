889b139d7603ab9982abdb266c258791ab8f91e6
Neha Sharma <tynehasharma@gmail.com>
2022-09-06 19:44:19 +0530

    Initial commit

---
+++ src/TripletSum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class TripletSum {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[]= new int[n];
+        for(int i=0; i<n;i++)
+            arr[i] =s.nextInt();
+        Arrays.sort(arr);
+        int target = s.nextInt();
+        tripletSum(arr,n,target);
+    }
+    public static void tripletSum(int []arr, int n , int target)
+    {
+        for(int i=0; i<n-2;i++)
+        {
+            int left = i+1;
+            int right =n-1;
+            while(left < right)
+            {
+                if(arr[i]+arr[left]+arr[right] == target)
+                {
+                    System.out.print(arr[i]+", "+arr[left]+" and "+arr[right]);
+                    System.out.println();
+                    left++;
+                    right--;
+                }
+                else if(arr[i]+arr[left]+arr[right] > target)
+                    right--;
+                else
+                    left++;
+            }
+        }
+    }
+}
---
+++ src/targetSum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class targetSum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n;i++)
+            arr[i] = s.nextInt();
+        int target = s.nextInt();
+        Arrays.sort(arr);
+        target(arr,n,target);
+    }
+    public static void target(int[] arr, int n, int target){
+        int start =0;
+        int end = n-1;
+        while(start <end)
+        {
+            int sum = arr[start]+arr[end];
+            if(sum > target)
+            {
+                end--;
+            }
+            else if(sum < target)
+            {
+                start++;
+            }
+            else if (sum == target){
+                System.out.println(arr[start]+" "+arr[end]);
+                start++;
+                end--;
+            }
+        }
+    }
+}
