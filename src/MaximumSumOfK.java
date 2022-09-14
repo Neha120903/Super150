5c7747f928e9f3622447f83ddc980728cdd1b4a4
Neha Sharma <tynehasharma@gmail.com>
2022-09-14 14:42:15 +0530

    Initial commit

---
+++ src/MaximumSumOfK.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MaximumSumOfK {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int[] arr={2,3,4,2,5,6,78,90};
+        int k=3;
+        System.out.println(maximumSum(arr,k));
+    }
+
+    public static int maximumSum(int[]arr, int k)
+    {
+        int sum=0;
+        for(int i=0; i<k; i++)
+            sum+=arr[i];
+        int ans=0;
+        for (int i = k; i < arr.length; i++) {
+            sum=(sum+arr[i])-arr[i-k];
+            ans=Math.max(sum,ans);
+        }
+        return ans;
+    }
+}
---
+++ src/SubarrayProductLessThanK.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SubarrayProductLessThanK {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int[] arr={1,2,4,3,5,2,3};
+        int k=10;
+        System.out.println(NoOfSubArray(arr,k));
+
+    }
+
+    public static int NoOfSubArray(int[]arr, int k)
+    {
+        int ans=0;
+        int strt =0;
+        int end=0;
+        int p=1;
+        while(end <arr.length)
+        {
+            //grow window
+            p *= arr[end];
+
+
+
+            //window shrink kro
+            while( p >=k && strt <= end)
+            {
+                p = p/arr[strt];
+                strt++;
+            }
+
+            //ans calculate kro
+            ans = ans+ (end-strt +1);
+            end++;
+        }
+        return ans;
+    }
+}
