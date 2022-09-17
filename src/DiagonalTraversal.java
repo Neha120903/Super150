ba1f83acdd658b6cd5e4f899e70dac9efab6edbf
Neha Sharma <tynehasharma@gmail.com>
2022-09-17 15:57:36 +0530

    Initial commit

---
+++ src/DiagonalTraversal.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class DiagonalTraversal {
+    public static void main(String[] args) {
+        int [][] arr= {{1,2,3},{4,5,6},{7,8,9}};
+        Diagonal(arr);
+    }
+    public static int[] Diagonal(int [][]arr)
+    {
+        int n =arr.length;
+        int m = arr[0].length;
+        int []ans = new int[n*m];
+        int k=0;
+        for (int i = 0; i <m+n-1 ; i++) {    //m+n-1 is number of diagonal
+            int r= 0;
+            int c=0;
+
+            if(i < m)
+            {
+                r=0;
+                c=i;
+            }
+            else{
+                c=m-1;
+                r=i-m+1;
+            }
+            ArrayList<Integer> al = new ArrayList<>();
+
+            while (r <n && c>=0)
+            {
+                al.add(arr[r][c]);
+                r++;
+                c--;
+            }
+            if(i%2==0)
+            {
+                for (int j = al.size()-1; j >=0; j++) {
+                    ans[k] = al.get(j);
+                    k++;
+                }
+            }
+            else{
+                for (int j = 0; j < al.size(); j++) {
+                    ans[k] =al.get(j);
+                    k++;
+                }
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/KartikBhaiyaAndStrings.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class KartikBhaiyaAndStrings {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = "abba";
+        int k=3;
+        int flipa = countSubstring(str,'a',k);
+        int flipb=countSubstring(str,'b',k);
+        System.out.println(Math.max(flipa,flipb));
+    }
+
+    public static int countSubstring(String s,char ch, int k)
+    {
+        int strt=0;
+        int end=0;
+        int ans=0;
+        int flip=0;
+        while(end <s.length())
+        {
+            //grow window
+            if(s.charAt(end) == ch){
+                flip++;
+            }
+            //shrink window
+            while(flip >k)
+            {
+                if (s.charAt(strt)==ch )
+                {
+                    flip--;
+
+                }
+                strt++;
+            }
+            ans=Math.max(ans,end-strt+1);
+            end++;
+        }
+        return ans;
+    }
+}
